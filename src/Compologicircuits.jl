b11b72d4e82658f91f37a4d9cc63e83e985a7687
tylerhanks <teh.hanks@gmail.com>
2021-12-02 10:28:59 -0500

    Implement 4bit adder

--- src/Compologicircuits.jl
+++ src/Compologicircuits.jl
@@ -23,20 +23,6 @@ show_diagram(d::WiringDiagram) = to_graphviz(d,
     )
 )
 
-# Syntactic representation of logic circuits
-B = Ob(FreeCartesianCategory, :B)
-NOT = Hom(:NOT, B, B)
-AND = Hom(:AND, B⊗B, B)
-OR = Hom(:OR, B⊗B, B)
-
-@present Circuits(FreeCartesianCategory) begin
-    B::Ob
-    NOT::Hom(B,B)
-    AND::Hom(B⊗B,B)
-    OR::Hom(B⊗B,B)
-    XOR::Hom(B⊗B,B)
-end
-
 # The skeleton of n-dimensional bool spaces which are the input/output types of logic circuits
 struct CircuitDom
     n::Int
@@ -62,7 +48,7 @@ iAND = Circuit(2, 1, x->[x[1] && x[2]])
 
 iOR = Circuit(2, 1, x->[x[1] || x[2]])
 
-iδ = Circuit(1, 2, x->[x[1], x[1]])
+#iδ = Circuit(1, 2, x->[x[1], x[1]])
 
 # Make circuits compositional by implementing them as an SMC instance
 @instance CartesianCategory{CircuitDom, Circuit} begin
@@ -105,18 +91,27 @@ iδ = Circuit(1, 2, x->[x[1], x[1]])
     proj2(A::CircuitDom, B::CircuitDom) = Circuit(A.n+B.n, B.n, x->x[A.n+1:end])
 end
 
+# Syntactic representation of logic circuits
+
+@present Circuits(FreeCartesianCategory) begin
+    B::Ob
+    NOT::Hom(B,B)
+    AND::Hom(B⊗B,B)
+    OR::Hom(B⊗B,B)
+    XOR::Hom(B⊗B,B)
+    FA::Hom(B⊗B⊗B,B⊗B)
+end
+
 # Functor from circuit diagrams to circuit implementations
-gens = Dict(B=>CircuitDom(1), NOT=>iNOT, AND=>iAND, OR=>iOR)
+gens = Dict(
+    Circuits[:B]=>CircuitDom(1), 
+    Circuits[:NOT]=>iNOT, 
+    Circuits[:AND]=>iAND, 
+    Circuits[:OR]=>iOR
+)
 Impl(expr) = functor((CircuitDom, Circuit), expr, generators=gens)
 
 # Make some circuits
-#=XOR = @program Circuits (x::B, y::B) begin
-    x1, x2 = δ(x)
-    y1, y2 = δ(y)
-    xnandy = NOT(AND(x1,y1))
-    xory = OR(x2,y2)
-    return AND(xnandy,xory)
-end=#
 
 XOR = @program Circuits (x::B, y::B) begin
     xnandy = NOT(AND(x,y))
@@ -125,5 +120,54 @@ XOR = @program Circuits (x::B, y::B) begin
 end
 
 XOR_expr = to_hom_expr(FreeCartesianCategory, XOR)
+
+iXOR = Impl(XOR_expr)
+
+# Print XOR truth table
+
+print_xor() = for i in Iterators.product([false, true], [false, true])
+    input = collect(i)
+    output = apply(iXOR, input)
+    println("XOR on $input = $output")
+end
+
+full_adder = @program Circuits (x::B, y::B, cin::B) begin
+    z = XOR(x,y)
+    s = XOR(z,cin)
+    cout = OR(AND(z,cin),AND(x,y))
+    return (s, cout)
+end
+
+AND = to_wiring_diagram(Circuits[:AND])
+NOT = to_wiring_diagram(Circuits[:NOT])
+OR = to_wiring_diagram(Circuits[:OR])
+total_full_adder = ocompose(full_adder, [XOR, XOR, AND, AND, OR])
+iFA = Impl(to_hom_expr(FreeCartesianCategory, total_full_adder))
+
+# Print Full Adder truth table
+
+print_fa() = for i in Iterators.product([false, true], [false, true], [false, true])
+    input = collect(i)
+    output = apply(iFA, input)
+    println("FA on $input = $output")
+end
+
+four_bit_adder = @program Circuits (cin::B, x1::B, x2::B, x3::B, x4::B, y1::B, y2::B, y3::B, y4::B) begin
+    s1, cout1 = FA(x4,y4,cin)
+    s2, cout2 = FA(x3,y3,cout1)
+    s3, cout3 = FA(x2,y2,cout2)
+    s4, cout = FA(x1,y1,cout3)
+    return (cout, s4, s3, s2, s1)
+end
+
+total_four_bit_adder = ocompose(four_bit_adder, [total_full_adder, total_full_adder, total_full_adder, total_full_adder])
+tfba_expr = to_hom_expr(FreeCartesianCategory, total_four_bit_adder)
+i4bitadder = Impl(tfba_expr)
+
+sum = apply(i4bitadder, 
+    Bool[0,      #carry in
+        0,1,1,1, #x
+        0,1,1,0] #y
+)
 
 #end
